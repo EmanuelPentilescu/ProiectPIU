b3384554ba146ac5cf14b9dac41471e42df0c1c3
EmanuelPentilescu <101604581+EmanuelPentilescu@users.noreply.github.com>
2022-03-29 13:35:41 +0300

    Add files via upload

---
+++ Cinematograf2/Cinematograf2/EntryPoint.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using LibrarieFilme;
+using NivelStocareDate;
+
+namespace Cinematograf
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
+            AdministrareFilme_FisierText adminFilme = new AdministrareFilme_FisierText(numeFisier);
+            int nrFilme = 0;
+            Film FilmNou = new Film();
+            adminFilme.GetFilme(out nrFilme);
+            string optiune;
+            do
+            {
+                Console.WriteLine("C: Citeste filmele de la tastatura:");
+                Console.WriteLine("F: Afiseaza numarul filmelor");
+                Console.WriteLine("A: Afisarea ultimului film introdus");
+                Console.WriteLine("S: Salveaza film in fisier");
+                Console.WriteLine("X: Exit");
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine();
+                switch (optiune.ToUpper())
+                {
+                    case "C":
+                        nrFilme++;
+                        FilmNou= CitesteFilmeTastatura();
+                        break;
+
+                    case "F":
+                        Film[] filme = adminFilme.GetFilme(out nrFilme);
+                        AfisareFilme(filme, nrFilme);
+                        break;
+
+                    case "A":
+                        AfisareFilm(FilmNou);
+                        break;
+
+                    case "S":
+                        int idFilm = nrFilme + 1;
+                        FilmNou.SetIdFilm(idFilm);
+                        adminFilme.AddFilm(FilmNou);
+                        nrFilme++;
+                        break;
+                    case "X":
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+                }
+
+            } while (optiune.ToUpper() != "X");
+
+            Console.ReadKey();
+        }
+
+        public static Film CitesteFilmeTastatura()
+        {
+            Console.WriteLine("Introduceti titlul filmului:");
+            string titlu = Console.ReadLine();
+
+            Film film = new Film(0,titlu);
+
+            return film;
+
+        }
+
+        public static void AfisareFilm(Film film)
+        {
+            string infoFilm = string.Format("Filmul cu id #{0} are numele: {1}",
+                film.GetIdFilm(),
+                film.GetTitluFilm() ?? "Necunoscut");
+        }
+        public static void AfisareFilme(Film[] filme, int nrFilme)
+        {
+            Console.WriteLine("Filmele sunt:");
+            for (int contor = 0; contor < nrFilme; contor++)
+            {
+                string infoFilm = string.Format("Filmul cu id-ul #{0} are titlul: {1}",
+                   filme[contor].GetIdFilm(),
+                   filme[contor].GetTitluFilm() ?? " NECUNOSCUT ");
+
+                Console.WriteLine(infoFilm);
+            }
+        }
+    }
+}
